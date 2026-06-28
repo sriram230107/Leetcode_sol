7c6661cb9b8ec9dc7d54b8740f69f1aa9461f5c9
Sri Ram <rsbb7703@gmail.com>
2026-06-28 19:13:50 +0530

    Time: 0 ms (100%), Space: 44.6 MB (65.6%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+            else if(nums[i]>target)
+            {
+                return i;
+            }
+            if(nums[nums.length-1]<target)
+            {
+                return nums.length;
+            }
+        }
+
+        return 0;
+    }
+}
