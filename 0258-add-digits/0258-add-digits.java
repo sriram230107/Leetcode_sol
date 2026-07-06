4aabc0029f3b7a204466b1b3a7fe4daa58a4bb47
Sri Ram <rsbb7703@gmail.com>
2026-07-06 22:53:30 +0530

    Time: 1 ms (97.89%), Space: 42.5 MB (62.83%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int addDigits(int num) {
+        while(true)
+        {
+            int sum=0;
+            while(!(num==0))
+            {
+                sum+=num%10;
+                num/=10;
+            }
+            if(sum<=9)
+            {
+                return sum;
+            }
+            else
+            {
+                num=sum;
+            }
+        }
+    }
+}
